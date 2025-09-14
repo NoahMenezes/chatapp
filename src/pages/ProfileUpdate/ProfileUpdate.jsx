f7ce7b8b4820c79cf3450a31c88e53cb8aa81500
NoahMenezes <2006noahmenezes@gmail.com>
2025-09-14 13:42:46 +0530

    ProfileUpdate.jsx code

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './ProfileUpdate.css';
+import assets from '../../assets/assets';
 
 const ProfileUpdate = () => {
   return (
     <div>
-      
+      <div className="profile">
+        <div className="profile-container">
+          <form action="">
+            <h3>Profile Details</h3>
+            <label htmlFor="avatar">
+              <input type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden/>
+              <img src={assets.avatar_icon} alt="" />
+              upload profile image
+            </label>
+            <input type="text" placeholder='Your name' required />
+            <textarea placeholder='Write profile bio' required></textarea>
+            <button type='submit'>Save</button>
+            
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
